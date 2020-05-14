52f9af694a5679017558a608abc999be127e1f64
zoedaemon <lindroy34@gmail.com>
2020-05-14 23:51:36 +0700

    root endpoint

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 )
+
+//Root path
+func root(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "API v0.1")
+	log.Printf("Request : %s \nPath : %s", request.Host, request.URL.Path)
+}
+
+//RegisterHandlers register all handlers API
+func registerHandlers() {
+
+	//register root path
+	http.HandleFunc("/", root)
+
+	//call it with localhost:8080 (without / ) to access root endpoint
+	Host := "localhost:8080"
+	log.Print("Listening On : ", Host)
+	log.Panic(http.ListenAndServe(Host, nil))
+}
 
 func main() {
 	log.Printf("Initialize Project...")
+	registerHandlers()
 }
