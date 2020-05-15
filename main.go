e6a2f6aee937898e636e83d1d9ba1e2b7bae9016
zoedaemon <lindroy34@gmail.com>
2020-05-16 05:22:33 +0700

    make more modular - use package  routers for SimpleAPI implementation

--- main.go
+++ main.go
@@ -2,40 +2,20 @@ package main
 
 import (
 	"log"
-	"net/http"
 
-	"github.com/zoedaemon/pretest/simpleapi"
+	"github.com/zoedaemon/pretest/routers"
 )
-
-//Root path
-func root(scope simpleapi.Scope) *simpleapi.Response {
-	return &simpleapi.Response{
-		//GOTCHA : errors.New(..) cannot show up in response, implement custom Error datatype instead
-		Error:        nil,           //&simpleapi.Error{Detail: "error sample"},
-		ResponseCode: http.StatusOK, //http.StatusInternalServerError,
-		Data:         map[string]string{"version": "API v0.1"},
-	}
-}
-
-//RegisterHandlers register all handlers API
-func registerHandlers() {
-
-	//create new SimpleAPI
-	SimpleAPI := simpleapi.New()
-
-	//register root path
-	//http.HandleFunc("/", root)
-	SimpleAPI.GetMethod("/", root)
-
-	//call it with localhost:8080 (without / ) to access root endpoint
-	Host := "localhost:8080"
-	log.Print("Listening On : ", Host)
-
-	//API Listen and Serve
-	SimpleAPI.Serve(Host)
-}
 
 func main() {
 	log.Printf("Initialize Project...")
-	registerHandlers()
+
+	//in Postman call it with localhost:8080 (without / ) to access root endpoint
+	Host := "localhost:8080"
+
+	//Create simple API
+	SimpleAPI := routers.RegisterHandlers(Host)
+
+	//API Listen and Serve
+	SimpleAPI.Serve(Host)
+	log.Print("Listening On : ", Host)
 }
